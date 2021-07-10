129be8eac147916beffaa7e1f6f667f05efb82ef
quynhnn-vn <nnquynh.0111@gmail.com>
2021-07-10 16:55:43 +0200

    Update styles

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -14,7 +14,7 @@ export const Topic = () => {
   return (
     <section>
       <img src={topic.icon} alt="" className="topic-icon" />
-      <h1>Topic: {topic.name}</h1>
+      <h1>{topic.name}</h1>
       <ul className="quizzes-list">
         {quizzesForTopic.map((quiz) => (
           <li className="quiz" key={quiz.id}>
